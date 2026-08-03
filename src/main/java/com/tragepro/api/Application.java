9ead9e29b738b5295d0694b2ef4f428b5666355b
dharam.dev <dharam.dev@devenv.local>
2026-08-03 23:19:00 +0530

    Test fix

--- src/main/java/com/tragepro/api/Application.java
+++ src/main/java/com/tragepro/api/Application.java
@@ -2,13 +2,14 @@ package com.tragepro.api;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+import io.github.resilience4j.springboot3.verifier.autoconfigure.SpringBoot3VerifierAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = SpringBoot3VerifierAutoConfiguration.class)
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Application {
 
